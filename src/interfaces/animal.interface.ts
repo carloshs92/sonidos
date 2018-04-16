dad5a484fe22c00aa900e3cfa8b4eb73699c4d72
Carlos Huamani <carlos.hs.92@gmail.com>
2018-04-15 23:51:45 -0500

    feat(home): list-view

---
+++ src/interfaces/animal.interface.ts
@@ -0,0 +1,7 @@
+export interface IAnimal {
+  nombre: string;
+  imagen: string;
+  audio: string;
+  duracion: number;
+  reproduciendo: boolean;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
+import { IAnimal } from '../../interfaces/animal.interface';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  animales: IAnimal[] = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
+    this.animales = ANIMALES.splice(0);
   }
 
 }
